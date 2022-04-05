992324168107515cb454334b89aa0bea080c9c9e
Clark Kampfe <clark.kampfe@gmail.com>
2022-04-05 13:59:02 -0500

    fix async_log example

--- examples/async_log.rs
+++ examples/async_log.rs
@@ -1,10 +1,5 @@
-//use futures::stream::{Stream, StreamExt};
-use multican::async_can_udp::AsyncUdpNetwork;
-use multican::async_socketcan::AsyncSocketCanNetwork;
 use multican::{CanBusType, CanConfig};
 use std::error::Error;
-use tokio::stream::StreamExt;
-use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -20,8 +15,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     ];
 
     let mut network = multican::from_config_async(cfg);
-    let mut can_stream = network.stream_bad();
-    while let Some(msg) = can_stream.next().await {
+    // let mut can_stream = network.stream_bad();
+    let mut can_stream = network.stream().await;
+    while let Some(msg) = can_stream.recv().await {
         println!("RX: {:?}", msg);
     }
 
