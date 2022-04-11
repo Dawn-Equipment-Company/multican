bc2d200d57e3df55b7c119bce70595190156b85d
Clark Kampfe <clark.kampfe@gmail.com>
2022-04-11 10:42:21 -0500

    async_socketcan: cleanup

--- src/async_socketcan.rs
+++ src/async_socketcan.rs
@@ -68,13 +68,8 @@ impl AsyncSocketCanNetwork {
 #[async_trait]
 impl AsyncCanNetwork for AsyncSocketCanNetwork {
     async fn send(&self, msg: CanMessage) -> Result<(), std::io::Error> {
-        // self.inner.lock().await.send(msg).await
-
-        // println!("sendingd ldkfjlsdjf {:?}", msg);
-
         let res = self
             .send_tx
-            // .send(Command::Send(msg))
             .send(Send(msg))
             .await
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::BrokenPipe, e));
@@ -85,15 +80,9 @@ impl AsyncCanNetwork for AsyncSocketCanNetwork {
     }
 
     async fn next(&self) -> Option<CanMessage> {
-        // println!("next called");
-        // let r = self.inner.lock().await.next().await;
-        // println!("next released");
-        // r
-
         let (reply_tx, reply_rx) = tokio::sync::oneshot::channel();
 
         self.next_tx
-            // .send(Command::Next(reply_tx))
             .send(Next(reply_tx))
             .await
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::BrokenPipe, e))
@@ -111,7 +100,6 @@ impl AsyncCanNetwork for AsyncSocketCanNetwork {
 
 /// SocketCAN adapter for mulitcan
 struct AsyncSocketCanNetworkImpl {
-    // pub socket: CANSocket,
     socket_tx: SplitSink<CANSocket, CANFrame>,
     socket_rx: SplitStream<CANSocket>,
     pub bus: u8,
@@ -122,7 +110,6 @@ impl AsyncSocketCanNetworkImpl {
         trace!("Sending {:?}", msg);
         let frame = CANFrame::new(msg.header, &msg.data, false, false)
             .expect("failed to convert can message to frame");
-        // self.socket_tx.write_frame(frame).unwrap();
         self.socket_tx.send(frame).await.unwrap();
         Ok(())
     }
@@ -139,44 +126,3 @@ impl AsyncSocketCanNetworkImpl {
         }
     }
 }
-
-// impl Stream for AsyncSocketCanNetworkImpl {
-//     type Item = CanMessage;
-//
-//     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
-//         println!("poll_next");
-//         match self.socket.poll_next_unpin(cx) {
-//             Poll::Ready(Some(Ok(frame))) => {
-//                 println!("got frame");
-//                 Poll::Ready(Some(CanMessage {
-//                     header: frame.id(),
-//                     data: frame.data().to_vec(),
-//                     bus: self.bus,
-//                 }))
-//             }
-//             Poll::Ready(Some(Err(_e))) => Poll::Ready(None),
-//             _ => Poll::Pending,
-//         }
-//     }
-// }
-
-// impl AsyncSocketCanNetworkImpl {
-//     /// id is the network interface prefix, usually 'can' or 'vcan'
-//     pub fn new(bus: u8, id: &'static str) -> Self {
-//         debug!("Initializing bus #{}", bus);
-//         let bus_id = format!("{}{}", id, bus);
-//         debug!("Opening bus number {} - id: {}", bus, bus_id);
-//         let socket = CANSocket::open(&bus_id).expect("Failed to open bus");
-//         /*socket
-//             .set_read_timeout(time::Duration::from_millis(100))
-//             .expect("Failed to set socketcan read timeout");
-//         socket
-//             .set_nonblocking(true)
-//             .expect("Failed to set socketcan socket to nonblocking");*/
-//         debug!("opened {}", bus_id);
-//         //let s2 = CANSocket::open("can2").expect("failed to open can2");
-//         //let mut x = socket.merge(s2);
-//         //socket.split();
-//         AsyncSocketCanNetworkImpl { socket, bus }
-//     }
-// }
